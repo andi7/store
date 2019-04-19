d8466d7a6455e1ab2aa4a516af2526a1628f1775
Andi Ndrecka <andi.ndrecka1@gmail.com>
2019-04-19 16:18:39 +0200

    transform before doing the beforeSave

--- src/hooks/useSave.js
+++ src/hooks/useSave.js
@@ -16,12 +16,12 @@ export default ({ path, name, validate, headers = {}, transform, method = 'post'
 
       const fullApiUrl = `${apiUrl}/${path.replace(/:(\w+)/, (_, group) => values[group])}`;
 
-      if (beforeSave) {
-        values = beforeSave(values);
-      }
-
       if (transform) {
         values = transform(values);
+      }
+
+      if (beforeSave) {
+        values = beforeSave(values);
       }
 
       setBusy(busyName);
